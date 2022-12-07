ee3e8e48203c1609b373e7d977cb774052e4d66e
Maksym-Tulay <maxtulay@gmail.com>
2022-12-07 12:51:13 +0200

    Task__2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const makeIngredientListItems = ingredients => {
+  return ingredients.map(ingredient => {
+    const ingredientListItem = document.createElement('li');
+    ingredientListItem.classList = 'item';
+    ingredientListItem.textContent = ingredient;
+    
+    return ingredientListItem;
+  });
+};
+
+const elements = makeIngredientListItems(ingredients);
+ingredientsList.append(...elements);
