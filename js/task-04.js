4d9954bc176e26baba441f530d8a7e4ba650f18b
Maksym-Tulay <maxtulay@gmail.com>
2022-12-08 15:23:15 +0200

    task_04__done

---
+++ js/task-04.js
@@ -0,0 +1,13 @@
+let counterValue = document.querySelector('#value');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const decrementButton = document.querySelector('[data-action="decrement"]');
+
+incrementButton.addEventListener("click", () => {
+    let result = Number(counterValue.innerHTML) + 1;
+    counterValue.innerHTML = result;
+});
+
+decrementButton.addEventListener("click", () => {
+    let result = Number(counterValue.innerHTML) - 1;
+    counterValue.innerHTML = result;
+});
