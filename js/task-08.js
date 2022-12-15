19d6089e4ccdc23c7eaa8ed7fc687ceb5cc62a02
Maksym-Tulay <maxtulay@gmail.com>
2022-12-15 08:30:22 +0200

    task-08__inProgress

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,36 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    const formElements = evt.currentTarget.elements;
+    const email = formElements.email;
+    const password = formElements.password;
+
+    const message = 'Всі поля повинні бути заповнені!';
+    // const formData = { email, password };
+
+    const formData = new FormData(evt.currentTarget).forEach((value, name) => {
+        console.log(name, value);
+    });;
+
+    // formData.forEach((value, name) => {
+    //     console.log(name, value);
+    // });
+    console.log(formData);
+
+    if (email.value === '' || password.value === '') {
+        alert(message);
+    } else {
+        return formData;
+    };
+
+    form.reset();
+
+    // console.log(formData);
+
+
+    
+}
