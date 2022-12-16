712f462e5ed4ea2f390a5112a0b76b402279ddbd
Maksym-Tulay <maxtulay@gmail.com>
2022-12-16 15:34:09 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,24 +5,20 @@ form.addEventListener('submit', onFormSubmit);
 function onFormSubmit(evt) {
     evt.preventDefault();
 
-    const formElements = evt.currentTarget.elements;
-    const email = formElements.email;
-    const password = formElements.password;
-
+    const email = evt.currentTarget.elements.email.value;
+    const password = evt.currentTarget.elements.password.value;
     const message = 'Всі поля повинні бути заповнені!';
-    // const formData = { email, password };
+    const formDataObject = {
+        email,
+        password
+    };
 
-    const formData = new FormData(evt.currentTarget);
-
-    if (email.value === '' || password.value === '') {
+    if (email === '' || password === '') {
         alert(message);
     } else {
-        return formData.forEach((value, name) => {
-            console.log({name, value})
-        });
+        return formDataObject && console.log(formDataObject);
     };
 
     form.reset();
-   
 };
 
