2f5cb819568eda40c705938e0c8c34bf021b4cf4
Maksym-Tulay <maxtulay@gmail.com>
2022-12-21 09:52:22 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,6 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
 
 const changeColorButton = document.querySelector('.change-color');
 const bodyTextColor = document.querySelector('.color');
@@ -9,9 +9,11 @@ const bodyBackgroundColor = document.querySelector('body');
 changeColorButton.addEventListener('click', changeBodyBackgroundColor);
 
 function changeBodyBackgroundColor(event) {
-  bodyBackgroundColor.classList = 'body-background';
-  bodyBackgroundColor.style.backgroundColor = getRandomHexColor();
+  let colorChanger = getRandomHexColor();
 
-  bodyTextColor.textContent = getRandomHexColor();
+  bodyBackgroundColor.classList = 'body-background';
+  bodyBackgroundColor.style.backgroundColor = colorChanger;
+
+  bodyTextColor.textContent = colorChanger;
 }
 
